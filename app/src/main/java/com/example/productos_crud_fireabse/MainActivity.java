478072110c56d6d38f345d59c456d63e920484ec
danielfernandoyepezvelez <danielfernandoyepezvelez>
2022-12-05 20:54:20 -0500

    Terminando El Proyecto De Firebase

--- app/src/main/java/com/example/productos_crud_fireabse/MainActivity.java
+++ app/src/main/java/com/example/productos_crud_fireabse/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.productos_crud_fireabse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,7 +11,14 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -50,58 +58,187 @@ public class MainActivity extends AppCompatActivity {
         jbtnCancel = findViewById(R.id.btnCancel);
     }
 
-    public void productSave(View view) {
-        code = jetCode.getText().toString();
-        name = jetName.getText().toString();
-        price = jetPrice.getText().toString();
-        quantity = jetQuantity.getText().toString();
+    private void userMessage(String message) {
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+    }
 
-        if (code.isEmpty() || name.isEmpty() || price.isEmpty() || quantity.isEmpty()) {
-            Toast.makeText(MainActivity.this, "Todos Los Campos Son Requeridos", Toast.LENGTH_LONG).show();
+    private Map<String, Object> userProduct(String code, String name, String price, String quantity, String category, String available) {
+        Map<String, Object> product = new HashMap<>();
+
+        product.put("Code", code);
+        product.put("Name", name);
+        product.put("Price", price);
+        product.put("Quantity", quantity);
+        product.put("Category", category);
+        product.put("Available", available);
+
+        return product;
+    }
+
+    private String productCategory() {
+        if (jrbAppliance.isChecked()) {
+            category = "Appliance";
+        } else if (jrbTechnology.isChecked()) {
+            category = "Technology";
+        } else if (jrbHome.isChecked()) {
+            category = "Home";
         } else {
-            /* Que Categoria Es El Producto */
-            if (jrbAppliance.isChecked()) {
-                category = "Appliance";
-            } else if (jrbTechnology.isChecked()) {
-                category = "Technology";
-            } else if (jrbHome.isChecked()) {
-                category = "Home";
-            } else {
-                category = "Other";
-            }
+            category = "Other";
+        }
 
-            // Create A New Product
-            Map<String, Object> product = new HashMap<>();
-            product.put("Code", code);
-            product.put("name", name);
-            product.put("price", price);
-            product.put("Quantity", quantity);
-            product.put("Category", category);
-            product.put("Available", "yes");
+        return category;
+    }
 
-            // Add a new document with a generated ID
-            /*db.collection("Stock")
-                            .add()
-
-            // Add a new document with a generated ID
-            db.collection("Campeonato")
-                    .add(equipo)
+    private void productSaveDB(String nameDB, Map<String, Object> productDocument, String changeInstruction) {
+        if (changeInstruction.equalsIgnoreCase("add")) {
+            db.collection(nameDB)
+                    .add(productDocument)
                     .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                         @Override
                         public void onSuccess(DocumentReference documentReference) {
                             //Log.d("", "DocumentSnapshot added with ID: " + documentReference.getId());
-                            Toast.makeText(MainActivity.this, "Documento Adicionado", Toast.LENGTH_LONG).show();
-                            limpiarCampos();
+                            userMessage("Documento Adicionado");
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
                             //Log.w("Error adding document", e);
-                            Toast.makeText(MainActivity.this, "Documento NO Adicionado", Toast.LENGTH_LONG).show();
+                            userMessage("Documento NO Adicionado");
                         }
-                    }); */
-
+                    });
+        } else {
+            db.collection(nameDB)
+                    .document(identDoc)
+                    .set(productDocument)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            //Log.d("", "DocumentSnapshot added with ID: " + documentReference.getId());
+                            userMessage("Documento Anulado Existosamente");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            //Log.w("Error adding document", e);
+                            userMessage("Documento NO Anulado");
+                        }
+                    });
         }
+    }
+
+    public void productSave(View view) {
+        String category;
+
+        code = jetCode.getText().toString();
+        name = jetName.getText().toString();
+        price = jetPrice.getText().toString();
+        quantity = jetQuantity.getText().toString();
+
+        if (code.isEmpty() || name.isEmpty() || price.isEmpty() || quantity.isEmpty()) {
+            userMessage("Todos Los Campos Son Requeridos");
+            jetCode.requestFocus();
+        } else {
+            /* Que Categoria Es El Producto */
+            category = productCategory();
+
+            // Create A New Product
+            Map<String, Object> product = userProduct(code, name, price, quantity, category, "yes");
+
+            // Add a new document with a generated ID
+            productSaveDB("Stock", product, "add");
+            cleanFields();
+        }
+    }
+
+    public void productQuery(View view) {
+        code = jetCode.getText().toString();
+
+        if (code.isEmpty()) {
+            userMessage("El Codigo Es Requerido");
+        } else {
+            db.collection("Stock")
+                    .whereEqualTo("Code", code)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document: task.getResult()) {
+
+                                    if (document.getString("Available").equalsIgnoreCase("no")) {
+                                        userMessage("El Registro Existe, Pero, Esta Inactivo");
+                                    } else {
+                                        identDoc = document.getId();
+
+                                        jetName.setText(document.getString("Name"));
+                                        jetPrice.setText(document.getString("Price"));
+                                        jetQuantity.setText(document.getString("Quantity"));
+
+                                        if (document.getString("Category").equalsIgnoreCase("Appliance")) {
+                                            jrbAppliance.setChecked(true);
+                                        } else if (document.getString("Category").equalsIgnoreCase("Technology")) {
+                                            jrbTechnology.setChecked(true);
+                                        } else if (document.getString("Category").equalsIgnoreCase("Home")) {
+                                            jrbHome.setChecked(true);
+                                        } else {
+                                            jrbOther.setChecked(true);
+                                        }
+
+                                        if (document.getString("Available").equalsIgnoreCase("yes")) {
+                                            jcbAvailable.setChecked(true);
+                                        } else {
+                                            jcbAvailable.setChecked(false);
+                                        }
+                                    }
+                                }
+                                userMessage("Producto Encontrado Existosamente");
+                            } else {
+                                userMessage("Producto NO Encontrado");
+                            }
+                        }
+                    });
+        }
+    }
+
+    public void productAnnul(View view) {
+        String category;
+        code = jetCode.getText().toString();
+        name = jetName.getText().toString();
+        price = jetPrice.getText().toString();
+        quantity = jetQuantity.getText().toString();
+
+        if (code.isEmpty() || name.isEmpty() || price.isEmpty() || quantity.isEmpty()) {
+            userMessage("Todos Los Campos Son Requeridos");
+        } else {
+            /* Que Categoria Es El Product */
+            category = productCategory();
+
+            // Create A Product
+            Map<String, Object> product = userProduct(code, name, price, quantity, category, "no");
+
+            // Annul A Product
+            productSaveDB("Stock", product, "set");
+            cleanFields();
+        }
+    }
+
+    public void productCancel(View view) {
+        cleanFields();
+    }
+
+    private void cleanFields() {
+        jetCode.setText("");
+        jetName.setText("");
+        jetPrice.setText("");
+        jetQuantity.setText("");
+
+        jrbAppliance.setChecked(true);
+        jrbTechnology.setChecked(false);
+        jrbHome.setChecked(false);
+        jrbOther.setChecked(false);
+
+        jcbAvailable.setChecked(false);
     }
 }
